910da03449496dbc8a943f4d29e236ee102573c4
Lakshan <lakshan.imantha02@gmail.com>
2025-08-05 12:44:11 +0530

    Initial Commit

---
+++ src/main/java/com/lakshan/ecowise/EcoWiseApplication.java
@@ -0,0 +1,13 @@
+package com.lakshan.ecowise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoWiseApplication.class, args);
+    }
+
+}
