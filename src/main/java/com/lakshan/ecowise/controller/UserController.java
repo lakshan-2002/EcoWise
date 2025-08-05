c57af4a04f499f220a6cb8d8dbbaf09c406dd4bc
Lakshan <lakshan.imantha02@gmail.com>
2025-08-05 12:51:25 +0530

    Implement User entity with repository, service, and controller layers

---
+++ src/main/java/com/lakshan/ecowise/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.lakshan.ecowise.controller;
+
+import com.lakshan.ecowise.entity.User;
+import com.lakshan.ecowise.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/addUser")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        user.setPassword(DigestUtils.sha256Hex(user.getPassword()));
+        userService.addNewUser(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User body){
+       var user = userService.getUserByEmail(body.getEmail());
+
+        if (user != null && user.getPassword().equals(body.getPassword())) {
+            return ResponseEntity.ok(user);
+        } else
+            return ResponseEntity.status(401).body("Invalid email or password");
+    }
+
+    @GetMapping("/getUser/{id}")
+    public User getUser(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/updateUser")
+    public void updateUser(@RequestBody User user) {
+        userService.updateUser(user);
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+    }
+}
