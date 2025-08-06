f4b6a181003e76cd65bedf95321fc6b31a5684da
Lakshan <lakshan.imantha02@gmail.com>
2025-08-06 13:22:56 +0530

    Add Recommendation entity

--- src/main/java/com/lakshan/ecowise/service/FoodWasteLogService.java
+++ src/main/java/com/lakshan/ecowise/service/FoodWasteLogService.java
@@ -11,14 +11,33 @@ import java.util.List;
 public class FoodWasteLogService {
 
     private final FoodWasteLogRepository foodWasteLogRepository;
+    private final RecommendationService recommendationService;
 
     @Autowired
-    public FoodWasteLogService(FoodWasteLogRepository foodWasteLogRepository) {
+    public FoodWasteLogService(FoodWasteLogRepository foodWasteLogRepository,
+                               RecommendationService recommendationService
+    ) {
         this.foodWasteLogRepository = foodWasteLogRepository;
+        this.recommendationService = recommendationService;
     }
 
     public void addFoodWasteLog(FoodWasteLog foodWasteLog) {
         foodWasteLogRepository.save(foodWasteLog);
+
+        String prompt = """
+                "I wasted %f %s of %s because they are %s. 
+                 Give me 3 recommendations related to reducing this type of food waste.
+                 Each recommendation must be in JSON format and contain only the following fields: 
+                 message and priority.
+                 Only return the JSON array. Do not include any explanation, markdown, or additional text."
+                """.formatted(
+                foodWasteLog.getQuantity(),
+                foodWasteLog.getUnit(),
+                foodWasteLog.getItemName(),
+                foodWasteLog.getReason()
+        );
+        recommendationService.saveRecommendations(prompt, foodWasteLog.getCategory(), foodWasteLog);
+
     }
 
     public List<FoodWasteLog> getAllFoodWasteLogs() {
@@ -34,6 +53,21 @@ public class FoodWasteLogService {
     public void updateFoodWasteLog(FoodWasteLog foodWasteLog) {
         if (foodWasteLogRepository.existsById(foodWasteLog.getId())) {
             foodWasteLogRepository.save(foodWasteLog);
+
+            String prompt = """
+                    "I wasted %f %s of %s because they are %s. 
+                     Give me 3 recommendations related to reducing this type of food waste.
+                     Each recommendation must be in JSON format and contain only the following fields: 
+                     message and priority.
+                     Only return the JSON array. Do not include any explanation, markdown, or additional text."
+                    """.formatted(
+                    foodWasteLog.getQuantity(),
+                    foodWasteLog.getUnit(),
+                    foodWasteLog.getItemName(),
+                    foodWasteLog.getReason()
+            );
+            recommendationService.saveRecommendations(prompt, foodWasteLog.getCategory(), foodWasteLog);
+
         } else {
             throw new RuntimeException("Food waste log not found with id: " + foodWasteLog.getId());
         }
