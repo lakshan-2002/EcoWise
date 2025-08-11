ed511860f395987119bf6a8e6d79c28a8a13ca3a
Lakshan <lakshan.imantha02@gmail.com>
2025-08-11 22:06:23 +0530

    Add cross origin annotation to avoid CORS issue allowing for all domains

--- src/main/java/com/lakshan/ecowise/controller/RecommendationController.java
+++ src/main/java/com/lakshan/ecowise/controller/RecommendationController.java
@@ -3,16 +3,14 @@ package com.lakshan.ecowise.controller;
 import com.lakshan.ecowise.entity.Recommendation;
 import com.lakshan.ecowise.service.RecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/recommendations")
+@CrossOrigin(origins = "*")
 public class RecommendationController {
 
     private final RecommendationService recommendationService;
