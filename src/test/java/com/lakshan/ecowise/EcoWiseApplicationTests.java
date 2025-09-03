ff02f19d0dd066c62407ef120ab6b9ebf22b4f95
Lakshan <lakshan.imantha02@gmail.com>
2025-09-03 22:26:00 +0530

    Explicitly tells EcoWiseApplicationTests class to load application-test.properties file

--- src/test/java/com/lakshan/ecowise/EcoWiseApplicationTests.java
+++ src/test/java/com/lakshan/ecowise/EcoWiseApplicationTests.java
@@ -2,8 +2,10 @@ package com.lakshan.ecowise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = EcoWiseApplication.class)
+@ActiveProfiles("test")
 class EcoWiseApplicationTests {
 
     @Test
