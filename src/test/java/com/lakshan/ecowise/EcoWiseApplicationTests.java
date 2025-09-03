05e1ffe8335bff531b06fc3394a9f4dd6686ff36
Lakshan <lakshan.imantha02@gmail.com>
2025-09-04 01:24:47 +0530

    Use H2 in-memory database for testing

--- src/test/java/com/lakshan/ecowise/EcoWiseApplicationTests.java
+++ src/test/java/com/lakshan/ecowise/EcoWiseApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = EcoWiseApplication.class)
-@ActiveProfiles("test")
+@ActiveProfiles("test")   // Spring tells to load application-test.properties for this test
 class EcoWiseApplicationTests {
 
     @Test
